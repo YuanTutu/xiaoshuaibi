c5c8c74ad773bb440b6db8ad1479953cec20d991
YuanTutu <38133429+YuanTutu@users.noreply.github.com>
2020-10-14 21:37:11 +0800

    Add files via upload

---
+++ bilibili.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import xlwt
+
+browser = webdriver.Chrome()
+browser.get("https://www.bilibili.com")
+
