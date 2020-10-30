befd73ca75b61c85e1dbb6b46788066a54eebeed
YuanTutu <38133429+YuanTutu@users.noreply.github.com>
2020-10-30 11:57:09 +0800

    Add files via upload

--- S33_maoyan.py
+++ S33_maoyan.py
@@ -3,14 +3,12 @@ import re
 from fontTools.ttLib import TTFont
 import os
 import xml.dom.minidom as xmldom
-
-
 #获取字体下载url
 def get_url():
     url = 'https://maoyan.com/board/1'
     headers = {
         'User-Agent': 'Mozilla / 5.0 (Windows NT 10.0;Win64;x64) AppleWebKit/537.36 (KHTML, likeGecko) Chrome/86.0.4240.111 Safari/537.36',
-        'Cookie':'__mta=107191741.1602835854858.1603691846160.1603696934234.19; uuid_n_v=v1; uuid=3A7BDE100F8711EBBFF0EDDBFC80D58083CA62FF1F854148BA2CA679F7A7B4EE; _lxsdk_cuid=1753076336525-0109a318bb0f71-c781f38-1fa400-17530763366c8; _lxsdk=3A7BDE100F8711EBBFF0EDDBFC80D58083CA62FF1F854148BA2CA679F7A7B4EE; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; __mta=107191741.1602835854858.1603691674304.1603691685143.13; _csrf=b8b24f12e6497b5d47548e7687edb08637d129f4b262903695852f584cc6ba5d; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1602835854,1603517847,1603691627,1603696934; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1603696934; _lxsdk_s=17563e9fd52-8d5-f86-9ff%7C%7C1'
+        'Cookie':'__mta=107191741.1602835854858.1603957073266.1603961124658.23; uuid_n_v=v1; uuid=3A7BDE100F8711EBBFF0EDDBFC80D58083CA62FF1F854148BA2CA679F7A7B4EE; _lxsdk_cuid=1753076336525-0109a318bb0f71-c781f38-1fa400-17530763366c8; _lxsdk=3A7BDE100F8711EBBFF0EDDBFC80D58083CA62FF1F854148BA2CA679F7A7B4EE; _lx_utm=utm_source%3Dbing%26utm_medium%3Dorganic; __mta=107191741.1602835854858.1603691674304.1603691685143.13; _csrf=82cfa51d7bb97ff2d8eeb91d3f7487cb1c0125f757d56a3c056f1bd99e99f262; Hm_lvt_703e94591e87be68cc8da0da7cbd0be2=1603517847,1603691627,1603696934,1603957073; Hm_lpvt_703e94591e87be68cc8da0da7cbd0be2=1603961123; _lxsdk_s=17573886d93-226-a14-c21%7C%7C2'
     }
     html = requests.get(url=url,headers=headers).text
     #print(html)
@@ -19,7 +17,6 @@ def get_url():
     # print(url)
     # print(font_file_name)
     return url,font_file_name
-
 #下载字体
 def download_woff(url,font_file_name):
     #固定命名，呆的一笔……
@@ -37,7 +34,6 @@ def woff_xml(font_file_name):
     base_font = TTFont("fonts/"+font_file_name)
     # 转换成xml格式
     base_font.saveXML("fonts/"+font_file_name+".xml")
-
 #猫眼字符库
 font_num_dit = [
     {'code':'uniE06C','num':'1'},{'code':'uniE845','num':'4'},{'code':'uniE824','num':'9'},
@@ -49,43 +45,23 @@ font_num_dit = [
     {'code':'uniF54D','num':'0'},{'code':'uniF65F','num':'9'},{'code':'','num':''},
 ]
 
-#print(font_num_dit)
-def get_num_dic(font_file_name):
-    num_dic = []
-    # with open('fonts/'+font_file_name,'wb') as f:
-    #     f.write(font_file)
-    old_font=TTFont('fonts/93d5fd748686d98bbe54a5e1a9d83bd72272.woff')
-    new_font=TTFont('fonts/'+font_file_name)
-    new_font_unicode_list = new_font.getGlyphOrder()[2:]#前面两个不是数字对应的编码去除掉
-
-    for i in range(len(new_font_unicode_list)):
-        news = new_font['glyf'][new_font_unicode_list[i]]
-        for j in range(len(new_font_unicode_list)):
-            olds = old_font['glyf'][font_num_dit[j]]['code']
-            if news == olds:
-                #对象相同，把数字给新的编码
-                num = font_num_dit[j]['num']
-                num_dic.append({'code':new_font_unicode_list[i].replace('uni','&#x').lower()+';','num':num})
-    return num_dic
 #解析xml提取出数字的编码
-def read_xml(font_file_name):
+def get_num(font_file_name):
     xmlfilepath = os.path.abspath("fonts/"+font_file_name+".xml")
     print(xmlfilepath)
     domobj = xmldom.parse(xmlfilepath)
     elementobj = domobj.documentElement
     subElementObj = elementobj.getElementsByTagName("TTGlyph")
     for i in range(1, 11):
-        namee = subElementObj[i].getAttribute("name")
-        print(i, namee)
-
-
+        num_code = subElementObj[i].getAttribute("name")
+        return num_code
+    #print(num_code)
 
 #主程序，挨个调用子程序
 def main():
     url,font_file_name = get_url()
     download_woff(url,font_file_name)
     woff_xml(font_file_name)
-    #get_num_dic(font_file_name)
-    read_xml(font_file_name)
+    num_code = get_num(font_file_name)
 if __name__ == '__main__':
     main()
