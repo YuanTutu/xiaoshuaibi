cad17d3e7b966b7d9cfb84b86efd717ba4b3b199
YuanTutu <38133429+YuanTutu@users.noreply.github.com>
2020-11-21 16:04:05 +0800

    Add files via upload

---
+++ S48_so_json.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+#https://mp.weixin.qq.com/s?__biz=MzU2ODYzNTkwMg==&mid=2247485338&idx=1&sn=5b4d6ed34a27ed5e81a3e5d8ccf8bee9&chksm=fc8bbf0bcbfc361d9fb3d676b968ec8f2d3eec13fd04b3f8103bffd604a0687ee445d0c64877&scene=178&cur_album_id=1321044729160859650#rd
+
+#设置代理时候记得电脑上面点一下保存
+#这节莫得代码，直接在代理工具上修改
+#需要注意的是操作步骤，在代理工具的web上面首先会被拦截，拦截时候第一遍点击 Resume，之后会收到拦截返回的数据，在这里进行修改，网页那边不用管，不然还是会请求到没修改的web
+#拦截之后点击修改时候，直接复制到pycharm并不靠谱，有大量的不换行内容，我选择直接在浏览器进行操作。
+#浏览器上在那个框直接Ctrl+a全选然后ctrl+F搜索一个个修改就可以了……
